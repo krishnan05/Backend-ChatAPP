c7290147fb6c7b8ddc6b8ad9261c231f3131c9cf
krishnan05 <aayushkr@iitbhilai.ac.in>
2024-07-07 19:57:08 +0530

    Changes for deployment

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const socket = require("socket.io");
 dotenv.config();
 app.use(cors());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://chatapp-krishnan.vercel.app'); // Replace with your frontend origin
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Adjust allowed headers as needed
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Adjust allowed methods as needed
+    res.header('Access-Control-Allow-Origin', 'https://chatapp-krishnan.vercel.app'); 
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
 });
 app.use(express.json());
